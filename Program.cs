48f1a5ffa97ee912f59df17571a90114ba17a488
KOKETSO23 <koketsosefefe@gmail.com>
2024-06-06 12:20:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first length");
+            double firstLength = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second length");
+            double secondLength = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter height");
+            double height = double.Parse(Console.ReadLine());
+
+            Trapezium trapezium = new Trapezium(firstLength, secondLength, height);
+            trapezium.Display();
+        }
+    }
+}
---
+++ Trapezium.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Trapezium
+    {
+        double a;
+        double b;
+        double h;
+
+        public Trapezium(double firstLength, double secondLength, double height)
+        {
+            a = firstLength;
+            b = secondLength;
+            h = height;
+        }
+        public double firstLength
+        {
+            get { return a; }
+            set { a = value; }
+        }
+        public double secondLength
+        {
+
+            get { return b; }
+            set { b = value; }
+
+        }
+
+        public double height
+        {
+            get { return h; }
+            set { h = value; }
+
+        }
+        public double GetArea()
+        {
+            return 0.5 * (a + b) * h;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("firstLength " + firstLength);
+            Console.WriteLine("secondLength" + secondLength);
+            Console.WriteLine("height" +  height);
+            Console.WriteLine("area" + GetArea());
+        }
+    }
+
+}
